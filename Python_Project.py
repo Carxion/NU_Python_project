2e60bd9f049dd419dbfb147aca4887d34f0ef7cc
Carxion <76987962+Carxion@users.noreply.github.com>
2022-09-10 22:18:15 -0700

    Added files via upload
    Couldnt figure how to set remote push up yet because I am new...

---
+++ Python_Project.py
@@ -0,0 +1,18 @@
+#Project:
+#Names: Alexander Anicete, Shaun Turner, Erik Jewell, Jose Diaz, Dominick Bolano
+#Class: Programming for Cybersecurity 216 
+#Instructor: Donald Smith
+#Psuedo Code Ideas for how script will be structured
+print("please enter prospective Password. Include 16 characters, Uppercase and lowercase letters, Numbers, and special characters, no dictionary words")
+
+Pass = input("Enter here:")
+# Password will be stored in pass variable
+# create rules that meet certain strong password criteria
+# for example:
+#if pass has no Upper case letters = return that password needs uppercase letters
+# if pass has no lowercase letters = return that password needs lowercase letters
+# if string contains no numbers = return that password needs numbers
+# if string contains no special characters = return that password needs special characters
+# if string contains dictionary words = return that password cannot be dictionary words\\
+#if string is less than 16 characters= return that password needs to be 16 characters or more
+# must def each of these as functions that will run and return if a password is valid. once meets all criteria can spit out that password successfully meets criteria
